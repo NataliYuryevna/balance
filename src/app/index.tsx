a8fc860ccd5b68a14079efe58e535814f5a937ac
n.proleeva <boltacheva.natali@gmail.com>
2024-05-24 13:37:47 +0200

    add webpack and alias

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Div} from "./index.style";
+
+function App() {
+  return (
+    <Div className="App">
+      Hello word
+    </Div>
+  );
+}
+
+export default App;
